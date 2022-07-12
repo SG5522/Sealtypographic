05dbaa2d766c96ea85e3831d48e00e7d3dcc90db
SG552 TSENG <sg552@djimage.com.tw>
2022-07-12 16:05:04 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 
 namespace dotnetcore
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {//test
             Console.WriteLine("Hello World!");
         }
     }
