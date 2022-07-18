5b0c400d80400fb30914e29f09cddd47aa0da368
SG552 <SG552@djimage.com.tw>
2022-07-18 18:32:25 +0800

    feat:取章排版DEMO
    此次動作
    1.使用Spire.PDF 顯示在網頁上
    
    尚未完成
    1.圖片(印章)拖曳到pdf上貼圖
    2.該圖片必須能白底變透明貼到pdf
    3.該PDF存檔必須是PDF/A

---
+++ DJSpire/SpirePDF.cs
@@ -0,0 +1,60 @@
+﻿using Spire.Pdf;
+using Spire.Pdf.Graphics;
+using System;
+using System.IO;
+
+namespace DJSpire
+{
+    public class SpirePDF
+    {
+        private readonly PdfDocument pdfDocument = new PdfDocument();
+        //public PdfDocument pdfDocument = new PdfDocument();
+        public void PDFOpen(string pdfPath)
+        {
+            //Load pdf document
+            
+            pdfDocument.LoadFromFile(pdfPath);
+
+            //Set view reference
+            pdfDocument.ViewerPreferences.CenterWindow = true;
+            pdfDocument.ViewerPreferences.DisplayTitle = false;
+            pdfDocument.ViewerPreferences.FitWindow = false;
+            pdfDocument.ViewerPreferences.HideMenubar = true;
+            pdfDocument.ViewerPreferences.HideToolbar = true;
+            pdfDocument.ViewerPreferences.PageLayout = PdfPageLayout.SinglePage;
+
+            //Save pdf file
+            //pdfDocument.SaveToFile("ViewerPreference_result.pdf");
+            //pdfDocument.Close();
+
+            //Launch the Pdf file
+            //PDFDocumentViewer("ViewerPreference_result.pdf");
+            
+        }
+        public MemoryStream PdfLoad(string pdfPath)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            
+            //Load pdf document
+            pdfDocument.LoadFromFile(pdfPath);
+            
+
+            //Set view reference
+            pdfDocument.ViewerPreferences.CenterWindow = true;
+            pdfDocument.ViewerPreferences.DisplayTitle = false;
+            pdfDocument.ViewerPreferences.FitWindow = false;
+            pdfDocument.ViewerPreferences.HideMenubar = true;
+            pdfDocument.ViewerPreferences.HideToolbar = true;
+            pdfDocument.ViewerPreferences.PageLayout = PdfPageLayout.SinglePage;
+
+            //在STAND2.0的環境下使用SaveToStream 會需要使用SkiaSharp
+            pdfDocument.SaveToStream(memoryStream);
+
+            return memoryStream;
+        }
+        public void PdfDocumentClose()
+        {
+            pdfDocument.Close();
+        }
+    }
+}
--- SealTypographicDemo/Controllers/HomeController.cs
+++ SealTypographicDemo/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SealTypographicDemo.Models;
 using System.Diagnostics;
+using DJSpire;
 
 namespace SealTypographicDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private SpirePDF spirePDF = new();
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -18,15 +20,33 @@ namespace SealTypographicDemo.Controllers
             return View();
         }
 
+        /*
         public IActionResult Privacy()
         {
             return View();
         }
+        */
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+        //[HttpPost]
+        public IActionResult PdfOpen()
+        {
+
+            //string FileName= "D:\\temp\\myPdf.pdf";
+            string pdfPath = @"D:\works\SealTypographic\SealTypographicDemo\wwwroot\pdf\test.pdf";
+
+            MemoryStream stream = spirePDF.PdfLoad(pdfPath);
+            stream.Position = 0;
+            spirePDF.PdfDocumentClose();
+            
+            return File(stream, "application/pdf");
         }
     }
 }
