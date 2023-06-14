085ba3c81d202bb3bdc3901e7ae10d3ba84f40fa
SG552 <SG552@djimage.com.tw>
2023-06-14 12:01:56 +0800

    feat:修改透通圖層處理

--- DJLib/OpenCvUtil.cs
+++ DJLib/OpenCvUtil.cs
@@ -10,58 +10,32 @@ namespace DJLib
     {
         public static void Transparent(string path)
         {
-            Mat outImage = new Mat();
-            Mat sourceImage = Cv2.ImRead(path);            
-            Cv2.CvtColor(sourceImage, outImage, ColorConversionCodes.BGR2BGRA);
-            Mat maskMat =  GetMaskMat(outImage, 235);
-            MergeMask(sourceImage, outImage, maskMat);
-            outImage.SaveImage("C://123.png");
-            //Mat[] mats = outImage.Split();
+            
+            //Mat tmpMat = new Mat();
+            //Mat alphaMat = new Mat();
+            Mat sourceImage = Cv2.ImRead(path);
+            Mat outMat = GetMat(sourceImage, 150);
+            outMat.SaveImage("C://123.png");            
         }
 
-        private static unsafe Mat GetMaskMat(Mat src, int threshold)
+        private static Mat GetMat(Mat srcMat,int threshold)
         {
-            Mat mat = new Mat(src.Rows, src.Cols, MatType.CV_8UC4);
-            for(int row = 0; row < src.Rows; row++)
-            {
-                Vec4b* srcRow = (Vec4b*)src.Ptr(row);
-                Vec4b* maskRow = (Vec4b*)mat.Ptr(row);
-                for(int col = 0; col < src.Cols; col++)
-                {
-                    Vec4b* pData = srcRow + col;
-                    Vec4b* maskPData = maskRow + col;
+            Mat tempMat = srcMat.CvtColor(ColorConversionCodes.BGR2BGRA);
+            //Mat[] splitMats = tempMat.Split();
 
-                    byte blue = pData->Item0;
-                    byte green = pData->Item1;
-                    byte red = pData->Item2;
-                    byte alpha = pData->Item3;
-                    
-                    if (blue <= threshold || green <= threshold || red <= threshold)
+            for (int row = 0; row < tempMat.Rows; row++)
+            {
+                for (int col = 0; col < tempMat.Cols; col++)
+                {
+                    Vec4b vec4b = tempMat.Get<Vec4b>(row, col);
+                    if (vec4b.Item0 > threshold && vec4b.Item1 > threshold && vec4b.Item2 > threshold)
                     {
-                        
-                        maskPData-> Item0 = 255;
-                        maskPData-> Item1 = 255;
-                        maskPData-> Item2 = 255;
-                        maskPData-> Item3 = 255;
-                    }
-                    else
-                    {                        
-                        maskPData->Item0 = 0;
-                        maskPData->Item1 = 0;
-                        maskPData->Item2 = 0;
-                        maskPData->Item3 = 0;
+                        vec4b.Item3 = 0;
+                        tempMat.Set(row, col, vec4b);
                     }
                 }
             }
-            return mat;
-        }
-
-        private static void MergeMask(Mat sourceImage, Mat outImage, Mat maskMat)
-        {            
-            Mat[] sourceImages = sourceImage.Split();
-            Mat[] maskMats = maskMat.Split();
-            Mat[] newMats = new Mat[] { sourceImages[0], sourceImages[1], sourceImages[2], maskMats[3] };
-            Cv2.Merge(newMats, outImage);
+            return tempMat;
         }
     }
 }
