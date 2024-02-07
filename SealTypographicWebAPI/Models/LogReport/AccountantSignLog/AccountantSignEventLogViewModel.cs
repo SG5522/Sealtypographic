4eb5208efc073861069a1274bca9347a54556f69
SG552 <sg552@djimage.com.tw>
2024-02-07 17:40:56 +0800

    fix:調整excel的欄位順序

--- SealTypographicWebAPI/Models/LogReport/AccountantSignLog/AccountantSignEventLogViewModel.cs
+++ SealTypographicWebAPI/Models/LogReport/AccountantSignLog/AccountantSignEventLogViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using DBEntities.Consts;
+using System.ComponentModel.DataAnnotations;
 
 namespace SealTypographicWebAPI.Models.LogReport.AccountantSignLog
 {
@@ -8,18 +9,21 @@ namespace SealTypographicWebAPI.Models.LogReport.AccountantSignLog
     public class AccountantSignEventLogViewModel : LogViewModelBase
     {
         /// <summary>
-        /// 審核狀態
-        /// </summary>
-        public ReviewStatus ReviewStatus { get; set; }
-
-        /// <summary>
         /// 會計師編號
         /// </summary>
+        [Display(Order = -2)]
         public string AccountantCode { get; set; }
 
         /// <summary>
         /// 會計師姓名
         /// </summary>
+        [Display(Order = -1)]
         public string AccountantName { get; set; }
+
+        /// <summary>
+        /// 審核狀態
+        /// </summary>
+        [Display(Order = 7)]
+        public ReviewStatus ReviewStatus { get; set; }
     }
 }
